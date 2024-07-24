0dc125d54740e4e8482e11af15522e90be53a1cd
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-07-24 11:32:42 +0700

    3. Android Studio Untuk Siswa SMK
    Menjalankan Java Pada Android Studio

--- HelloWorld/app/src/main/java/com/komputerkit/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/komputerkit/helloworld/MainActivity.java
@@ -20,5 +20,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        tampil();
+    }
+    public void tampil(){
+        System.out.println("Belajar Android");
     }
 }
