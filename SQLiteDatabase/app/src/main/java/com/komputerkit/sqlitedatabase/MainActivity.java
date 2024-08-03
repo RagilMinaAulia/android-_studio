84c1abbbc707195340797799e8ddd1d11ef392d2
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-08-03 20:04:39 +0700

    37. SQL Update Data

--- SQLiteDatabase/app/src/main/java/com/komputerkit/sqlitedatabase/MainActivity.java
+++ SQLiteDatabase/app/src/main/java/com/komputerkit/sqlitedatabase/MainActivity.java
@@ -77,7 +77,15 @@ public class MainActivity extends AppCompatActivity {
                     pesan("insert gagal");
                 }
             }else{
-                pesan("update");
+                String sql = "UPDATE tblbarang\n" +
+                        "SET barang = \'"+barang+"', stok = "+stok+", harga = "+harga+"\n" +
+                        "WHERE idbarang = "+idbarang+"";
+                if (db.runSQL(sql)){
+                    pesan("data sudah diubah");
+                    selectData();
+                }else {
+                    pesan("data tidak bisa diubah");
+                }
             }
         }
         etBarang.setText("");
