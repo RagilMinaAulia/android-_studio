e64246b05588e94967583c25061bcf0ac822f678
Ragil Mina Aulia <140393700+RagilMinaAulia@users.noreply.github.com>
2024-08-03 18:56:44 +0700

    33. SQL Select Data 2

--- SQLiteDatabase/app/src/main/java/com/komputerkit/sqlitedatabase/BarangAdapter.java
+++ SQLiteDatabase/app/src/main/java/com/komputerkit/sqlitedatabase/BarangAdapter.java
@@ -1,6 +1,7 @@
 package com.komputerkit.sqlitedatabase;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -22,13 +23,16 @@ public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_barang,viewGroup,false);
+        return new ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.tvBarang.setText(barangList.get(i).getBarang());
+        viewHolder.tvStok.setText(barangList.get(i).getStok());
+        viewHolder.tvHarga.setText(barangList.get(i).getHarga());
     }
 
     @Override
@@ -43,7 +47,7 @@ public class BarangAdapter extends RecyclerView.Adapter<BarangAdapter.ViewHolder
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            tvBarang = itemView.findViewById(R.id.tvBarang);
+            tvBarang = itemView.findViewById(R.id.rcvBarang);
             tvStok = itemView.findViewById(R.id.tvStok);
             tvHarga = itemView.findViewById(R.id.tvHarga);
             tvMenu = itemView.findViewById(R.id.tvMenu);
